75fd8c945be13864513dbcb033d09c2edab4edfa
insulineru <insuline.ru@gmail.com>
2020-04-24 16:15:49 +0300

    feat: add base offer layout (mobile)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,199 @@
 <template>
   <div id="app">
+    <header>
+      <img src="@/assets/images/bg-header-mobile.svg">
+    </header>
+    <main>
+      <job-offer v-for="job in jobs" :key="job.id" :job="job" />
+    </main>
   </div>
 </template>
 
 <script>
+import JobOffer from './components/JobOffer.vue';
+
 export default {
   name: 'App',
+  components: {
+    JobOffer,
+  },
+  data() {
+    return {
+      jobs: [
+        {
+          id: 1,
+          company: 'Photosnap',
+          logo: 'photosnap.svg',
+          new: true,
+          featured: true,
+          position: 'Senior Frontend Developer',
+          role: 'Frontend',
+          level: 'Senior',
+          postedAt: '1d ago',
+          contract: 'Full Time',
+          location: 'USA Only',
+          languages: ['HTML', 'CSS', 'JavaScript'],
+        },
+        {
+          id: 2,
+          company: 'Manage',
+          logo: 'manage.svg',
+          new: true,
+          featured: true,
+          position: 'Fullstack Developer',
+          role: 'Fullstack',
+          level: 'Midweight',
+          postedAt: '1d ago',
+          contract: 'Part Time',
+          location: 'Remote',
+          languages: ['Python'],
+          tools: ['React'],
+        },
+        {
+          id: 3,
+          company: 'Account',
+          logo: 'account.svg',
+          new: true,
+          featured: false,
+          position: 'Junior Frontend Developer',
+          role: 'Frontend',
+          level: 'Junior',
+          postedAt: '2d ago',
+          contract: 'Part Time',
+          location: 'USA Only',
+          languages: ['JavaScript'],
+          tools: ['React', 'Sass'],
+        },
+        {
+          id: 4,
+          company: 'MyHome',
+          logo: 'myhome.svg',
+          new: false,
+          featured: false,
+          position: 'Junior Frontend Developer',
+          role: 'Frontend',
+          level: 'Junior',
+          postedAt: '5d ago',
+          contract: 'Contract',
+          location: 'USA Only',
+          languages: ['CSS', 'JavaScript'],
+        },
+        {
+          id: 5,
+          company: 'Loop Studios',
+          logo: 'loop-studios.svg',
+          new: false,
+          featured: false,
+          position: 'Software Engineer',
+          role: 'FullStack',
+          level: 'Midweight',
+          postedAt: '1w ago',
+          contract: 'Full Time',
+          location: 'Worldwide',
+          languages: ['JavaScript'],
+          tools: ['Ruby', 'Sass'],
+        },
+        {
+          id: 6,
+          company: 'FaceIt',
+          logo: 'faceit.svg',
+          new: false,
+          featured: false,
+          position: 'Junior Backend Developer',
+          role: 'Backend',
+          level: 'Junior',
+          postedAt: '2w ago',
+          contract: 'Full Time',
+          location: 'UK Only',
+          tools: ['RoR'],
+        },
+        {
+          id: 7,
+          company: 'Shortly',
+          logo: 'shortly.svg',
+          new: false,
+          featured: false,
+          position: 'Junior Developer',
+          role: 'Frontend',
+          level: 'Junior',
+          postedAt: '2w ago',
+          contract: 'Full Time',
+          location: 'Worldwide',
+          languages: ['HTML', 'JavaScript'],
+          tools: ['Sass'],
+        },
+        {
+          id: 8,
+          company: 'Insure',
+          logo: 'insure.svg',
+          new: false,
+          featured: false,
+          position: 'Junior Frontend Developer',
+          role: 'Frontend',
+          level: 'Junior',
+          postedAt: '2w ago',
+          contract: 'Full Time',
+          location: 'USA Only',
+          languages: ['JavaScript'],
+          tools: ['Vue, Sass'],
+        },
+        {
+          id: 9,
+          company: 'Eyecam Co.',
+          logo: 'eyecam-co.svg',
+          new: false,
+          featured: false,
+          position: 'Full Stack Engineer',
+          role: 'Fullstack',
+          level: 'Midweight',
+          postedAt: '3w ago',
+          contract: 'Full Time',
+          location: 'Worldwide',
+          languages: ['JavaScript', 'Python'],
+          tools: ['Django'],
+        },
+        {
+          id: 10,
+          company: 'The Air Filter Company',
+          logo: 'the-air-filter-company.svg',
+          new: false,
+          featured: false,
+          position: 'Front-end Dev',
+          role: 'Frontend',
+          level: 'Junior',
+          postedAt: '1mo ago',
+          contract: 'Part Time',
+          location: 'Worldwide',
+          languages: ['JavaScript'],
+          tools: ['React', 'Sass'],
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Spartan', sans-serif;
+  font-size: 15px;
+
+}
+
+#app {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  background-color: hsl(180, 52%, 96%);
+}
+
+header {
+  background-color: hsl(180, 29%, 50%);
+}
+
+main {
+  padding-top: 34px;
+}
+</style>
---
+++ src/components/JobOffer.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="job-offer">
+    <img :src="require(`@/assets/images/${job.logo}`)" class="logo">
+    <div class="row">
+      <h3 class="title">
+        {{ job.company }}
+      </h3>
+      <p class="badge badge-new" v-if="job.new">New!</p>
+      <p class="badge badge-featured" v-if="job.featured">Featured</p>
+    </div>
+    <div class="row">
+      <p class="position">{{ job.position }}</p>
+    </div>
+    <div class="row">
+      <span class="label">{{ job.postedAt }}</span>
+      <span class="label">{{ job.contract }}</span>
+      <span class="label">{{ job.location }}</span>
+    </div>
+    <hr class="divider">
+    <div class="row">
+      <p class="tag">{{ job.role }}</p>
+      <p class="tag">{{ job.level }}</p>
+      <p class="tag" v-for="tool in job.tools" :key="tool"
+      >{{ tool }}</p>
+      <p class="tag" v-for="language in job.languages" :key="language"
+      >{{ language }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JobOffer',
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.job-offer {
+  margin: 0 24px 40px;
+  padding: 24px;
+  padding-top: 37px;
+  background-color: #fff;
+  position: relative;
+}
+
+.logo {
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  top: 0;
+  left: 24px;
+  transform: translateY(-50%);
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-bottom: 8px;
+}
+
+.title {
+  margin: 0;
+  margin-right: 16px;
+  padding: 0;
+  font-weight: 700;
+  font-size: 13px;
+  color: hsl(180, 29%, 50%);
+}
+
+.badge {
+  border-radius: 12px;
+  padding: 6px 8px 4px;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 11px;
+  line-height: 14px;
+  color: hsl(0, 0%, 100%);
+  margin: 0;
+  margin-right: 8px;
+}
+
+.badge-new {
+  background-color: hsl(180, 29%, 50%);
+}
+
+.badge-featured {
+  background-color: hsl(180, 14%, 20%);
+}
+
+.position {
+  margin: 0;
+  font-weight: 700;
+  line-height: 1.6;
+  color: hsl(180, 14%, 20%);
+}
+
+.label {
+  position: relative;
+  font-size: 13px;
+  line-height: 24px;
+  padding-right: 20px;
+  color: hsl(180, 8%, 52%);
+}
+
+.label:last-child {
+  padding-right: 0;
+}
+
+.label:not(:last-child)::after {
+  content: '';
+  position: absolute;
+  right: 8px;
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 50%;
+  width: 4px;
+  height: 4px;
+  background-color: hsl(180, 8%, 52%);
+}
+
+.divider {
+  border: 0;
+  height: 1px;
+  background-color: hsl(180, 8%, 52%);
+}
+
+.tag {
+  padding: 4px 8px;
+  background-color: hsl(180, 52%, 96%);
+  color: hsl(180, 29%, 50%);
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 24px;
+  border-radius: 4px;
+  margin-right: 16px;
+}
+</style>
